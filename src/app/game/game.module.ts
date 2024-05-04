d841fdc2028acb156cfcd527a8fda7bbd77b5749
Bob Yoshi <alexchernyak@gmail.com>
2024-05-04 11:03:02 -0700

    ng 13

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -14,21 +14,21 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  declarations: [
-    GameComponent,
-    SpotDialogComponent,
-    ConfirmationDialogComponent,
-    LicensePlateFilterPipe],
-  imports: [
-    CommonModule,
-    GameRoutingModule,
-    ReactiveFormsModule,
-    GeneralModule,
-    MatCheckboxModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule
-  ],
-  entryComponents: [SpotDialogComponent, ConfirmationDialogComponent]
+    declarations: [
+        GameComponent,
+        SpotDialogComponent,
+        ConfirmationDialogComponent,
+        LicensePlateFilterPipe
+    ],
+    imports: [
+        CommonModule,
+        GameRoutingModule,
+        ReactiveFormsModule,
+        GeneralModule,
+        MatCheckboxModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule
+    ]
 })
 export class GameModule { }
