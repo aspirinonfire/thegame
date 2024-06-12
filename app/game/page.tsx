33d61151a5d2d382bd350b6b165065f87dbdcd7d
Bob Yoshi <alexchernyak@gmail.com>
2024-06-11 17:32:48 -0700

    added usmap and cleanup

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,21 +1,30 @@
 "use client"
 import { useState } from 'react';
 import PlatePicker from './platepicker';
+import UsMap, { SpottedLicensePlates } from '../common/usmap';
 
 export default function Game() {
   const [showPicker, setShowPicker] = useState(false);
 
+  // todo read from state
+  const plates: SpottedLicensePlates = {
+    "CA": true,
+    "AL": 1.0
+  };
+
   return (
-    <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-      <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-        ...game
-      </p>
-      <button
-        className="bg-blue-900 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowPicker(true)}>
-        I spy...
-      </button>
+    <div className="flex flex-col gap-6 rounded-lg bg-gray-100">
+      <div className="px-4">
+        <div>
+          <UsMap plateSpots={plates} onMapClick={() => setShowPicker(true)}/>
+        </div>
+        <div className="py-5">
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            ...game
+          </p>
+        </div>
+      </div>
+
       { showPicker ? (<PlatePicker setShowPicker={(isShown: boolean) => setShowPicker(isShown)} /> ) : null }
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,25 +18,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-          <nav className="inline-flex items-center gap-20">
-            <h1 className="text-2xl">License Plate Game</h1>
-            <div className="inline-flex items-center gap-4">
-              <Link href="/game">Game</Link>
-              <Link href="/history">History</Link>
-              <Link href="/about">About</Link>
-              <a className="text white p-4">Share App</a>
+        <div className="flex-col min-h-screen">
+          <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4">
+            <nav className="inline-flex items-center gap-20">
+              <h1 className="text-2xl">License Plate Game</h1>
+              <div className="inline-flex items-center gap-4">
+                <Link href="/game">Game</Link>
+                <Link href="/history">History</Link>
+                <Link href="/about">About</Link>
+                <a className="text white p-4">Share App</a>
+              </div>
+            </nav>
+          </div>
+
+          <main className="flex flex-row grow">
+            <div className="mt-4 grow">
+              {children}
             </div>
-          </nav>
+          </main>
         </div>
-        <main className="flex flex-col p-6">
-          <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-            {children}
-          </div>
-          <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-            <small>by Alex Chernyak</small>
-          </div>
-        </main>
       </body>
     </html>
   );
