25357717b9940fb827af0475159ae9a286cdad3b
Alex Chernyak <alexchernyak@gmail.com>
2024-06-21 14:52:05 -0700

    stuff

--- app/game/platepicker.tsx
+++ app/game/platepicker.tsx
@@ -89,7 +89,7 @@ export default function PlatePicker({ isShowPicker, setShowPicker, saveNewPlateD
       <svg width="50px" height="50px" className="fill-gray-400" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <title>ic_fluent_checkbox_unchecked_24_regular</title>
         <desc>Created with Sketch.</desc>
-        <g id="🔍-Product-Icons" stroke="none" strokeWidth="1" fill-rule="evenodd">
+        <g id="🔍-Product-Icons" stroke="none" strokeWidth="1" fillRule="evenodd">
           <path d="M5.75,3 L18.25,3 C19.7687831,3 21,4.23121694 21,5.75 L21,18.25 C21,19.7687831 19.7687831,21 18.25,21 L5.75,21 C4.23121694,21 3,19.7687831 3,18.25 L3,5.75 C3,4.23121694 4.23121694,3 5.75,3 Z M5.75,4.5 C5.05964406,4.5 4.5,5.05964406 4.5,5.75 L4.5,18.25 C4.5,18.9403559 5.05964406,19.5 5.75,19.5 L18.25,19.5 C18.9403559,19.5 19.5,18.9403559 19.5,18.25 L19.5,5.75 C19.5,5.05964406 18.9403559,4.5 18.25,4.5 L5.75,4.5 Z" id="🎨Color">
           </path>
         </g>
@@ -102,7 +102,7 @@ export default function PlatePicker({ isShowPicker, setShowPicker, saveNewPlateD
       <svg width="50px" height="50px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <title>ic_fluent_checkbox_checked_24_regular</title>
         <desc>Created with Sketch.</desc>
-        <g id="🔍-Product-Icons" stroke="none" strokeWidth="1" fill-rule="evenodd">
+        <g id="🔍-Product-Icons" stroke="none" strokeWidth="1" fillRule="evenodd">
           <path d="M18.25,3 C19.7687831,3 21,4.23121694 21,5.75 L21,18.25 C21,19.7687831 19.7687831,21 18.25,21 L5.75,21 C4.23121694,21 3,19.7687831 3,18.25 L3,5.75 C3,4.23121694 4.23121694,3 5.75,3 L18.25,3 Z M18.25,4.5 L5.75,4.5 C5.05964406,4.5 4.5,5.05964406 4.5,5.75 L4.5,18.25 C4.5,18.9403559 5.05964406,19.5 5.75,19.5 L18.25,19.5 C18.9403559,19.5 19.5,18.9403559 19.5,18.25 L19.5,5.75 C19.5,5.05964406 18.9403559,4.5 18.25,4.5 Z M10,14.4393398 L16.4696699,7.96966991 C16.7625631,7.6767767 17.2374369,7.6767767 17.5303301,7.96966991 C17.7965966,8.23593648 17.8208027,8.65260016 17.6029482,8.94621165 L17.5303301,9.03033009 L10.5303301,16.0303301 C10.2640635,16.2965966 9.84739984,16.3208027 9.55378835,16.1029482 L9.46966991,16.0303301 L6.46966991,13.0303301 C6.1767767,12.7374369 6.1767767,12.2625631 6.46966991,11.9696699 C6.73593648,11.7034034 7.15260016,11.6791973 7.44621165,11.8970518 L7.53033009,11.9696699 L10,14.4393398 L16.4696699,7.96966991 L10,14.4393398 Z" id="🎨Color">
           </path>
         </g>
@@ -113,13 +113,21 @@ export default function PlatePicker({ isShowPicker, setShowPicker, saveNewPlateD
   function renderCheckboxes() {
     return territoriesToRender
       .map((territory) => (
-        <div key={GetTerritoryKey(territory)} onClick={e => handleCheckboxChange(territory, e)} className="flex flex-row grow gap-3 text-black justify-start items-center">
+        <div key={GetTerritoryKey(territory)}
+          onClick={e => handleCheckboxChange(territory, e)}
+          className="flex flex-row grow gap-3 text-black justify-start items-center">
           <div className="flex w-1/5 md:w-1/4 justify-end">
             { !!formData[GetTerritoryKey(territory)] ? renderCheckedBox() : renderUncheckedBox()}
           </div>
           <div className="flex flex-col flex-grow justify-start">
             {territory.country == "US" ?
-              (<Image src={`/${territory.licensePlateImgs[0]}`} alt={territory.shortName} width={300} height={500} />) :
+              (<Image src={`/${territory.licensePlateImgs[0]}`}
+                alt={territory.shortName}
+                width="300"
+                height="0"
+                placeholder="blur"
+                blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEBLAEsAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAHAAoDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDet/HOifZQVihBjTaM2QBAC8Drg9ucc4/Gqc3jfw9JNI5tlO5icixQf1ooqnK/QLn/2Q=="
+                className="w-300 h-auto" />) :
               (<h1 className="text-2xl">{territory.longName} ({territory.country})</h1>)}
           </div>
         </div>
