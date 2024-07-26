cbd73e2916ae05dd32eba1c89db9ff50e426b089
Alex Chernyak <alexchernyak@gmail.com>
2024-07-26 14:55:04 -0700

    pulumi wip

---
+++ backend/TheGame.Pulumi/Program.cs
@@ -0,0 +1,29 @@
+﻿using Pulumi.AzureNative.Resources.V20240301;
+using Pulumi.AzureNative.Web.V20230101;
+using Pulumi.AzureNative.Web.V20230101.Inputs;
+using System;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(async () =>
+{
+  var config = new Pulumi.Config();
+  var subscriptionId = config.Require("subscriptionId");
+  var resGroupName = config.Require("resourceGroupName");
+
+  // Get reference to an existing resource group
+  var getResGroupArgs = new GetResourceGroupArgs
+  {
+    ResourceGroupName = resGroupName
+  };
+
+  var resGroup = await GetResourceGroup.InvokeAsync(getResGroupArgs)
+    ?? throw new InvalidOperationException($"Resource group {resGroupName} was not found!");
+
+  // TODO create ACA resources here...
+
+  // Export outputs here
+  return new Dictionary<string, object?>
+  {
+    ["ResourceGroupId"] = resGroup?.Id
+  };
+});
---
+++ backend/TheGame.Pulumi/README.md
@@ -0,0 +1,10 @@
+# The Game IaC with Pulumi
+
+## Setup
+### For local state management:
+`pulumi login --local`
+`mkdir && cd`
+`pulumi new csharp`
+
+### For self-managed
+[Self Managed backend](https://www.pulumi.com/docs/concepts/state/#using-a-self-managed-backend)
