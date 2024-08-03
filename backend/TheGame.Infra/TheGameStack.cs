a9c575bcbd0799284c8f324d05de59ccef7f5c1b
Alex Chernyak <alexchernyak@gmail.com>
2024-08-02 18:15:20 -0700

    role deployment improvements

--- backend/TheGame.Infra/TheGameStack.cs
+++ backend/TheGame.Infra/TheGameStack.cs
@@ -192,40 +192,42 @@ public sealed class TheGameStack
 
   private void CreateSqlUserForContainerAppAndAssignRoles(ContainerApp containerApp, ExistingAzureResources existingResources)
   {
-    // Use the managed identity to execute a SQL script
-    var entraAppObjectId = containerApp.Identity.Apply(identity => identity?.PrincipalId);
-
-    var gameAppIdentityName = entraAppObjectId.Apply(identityObjectId =>
+    // Managed identity name (not id) _usually_ matches ACA name. Querying identity name _from_ principal id requires elevated privilidges
+    // Not gonna touch this unless it becomes a problem in the future.
+    var entraAppObjectId = containerApp.Name.Apply(async acaIdentityName =>
     {
-      if (string.IsNullOrEmpty(identityObjectId))
-      {
-        throw new InvalidOperationException("Unable to add SQL Users because Container App was created without System Managed Identity.");
-      }
+      // Create user script will authenticate with SQL server using it identity established with 'az login'
+      // we'll use 2min timeout to allow serverless db to back to life. container app has db retries enabled so it can tolerate shorter timeout
+      var connectionString = GetSqlConnectionString(existingResources, "Active Directory Default", 120);
 
-      var spRegistration = ServicePrincipal.Get(name: "gameapp-aca-sp-name", identityObjectId);
+      await AssignSqlRwToExternalIdentity(connectionString, acaIdentityName);
 
-      spRegistration.DisplayName.Apply(async acaIdentityName =>
-      {
-        // Create user script will authenticate with SQL server using it identity established with 'az login'
-        // we'll use 2min timeout to allow serverless db to back to life. container app has db retries enabled so it can tolerate shorter timeout
-        var connectionString = GetSqlConnectionString(existingResources, "Active Directory Default", 120);
-
-        await AssignSqlRwToExternalIdentity(connectionString, acaIdentityName);
-      });
-      
-      return true;
     });
   }
 
   public static async Task AssignSqlRwToExternalIdentity(string connectionString, string externalUserName)
   {
     using var connection = new SqlConnection(connectionString);
-    await connection.OpenAsync();
+    
+    // TODO figure out better resilience solution
+    for (var attempt = 0; attempt < 10; attempt++)
+    {
+      try
+      {
+        await connection.OpenAsync();
+        break;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Failed to connect to SQL Server: {ex.GetType().Name}: {ex.Message}");
+      }
+      await Task.Delay(TimeSpan.FromSeconds(10));
+    }
 
     using var transaction = connection.BeginTransaction();
     try
     {
-      Console.WriteLine("Attempting to add ACA identity to SQL user roles...");
+      Console.WriteLine($"Attempting to add ACA identity {externalUserName} to SQL user roles...");
       using var command = connection.CreateCommand();
       command.Transaction = transaction;
       // SQL statement needs to be idempotent!
