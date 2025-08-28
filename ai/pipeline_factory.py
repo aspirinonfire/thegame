0233ff9f2b1b7ca18cd325016c0255fae7272b48
Alex Chernyak <alexchernyak@gmail.com>
2025-08-27 22:26:22 -0700

    switched to grid search in hunt for better model params

--- ai/pipeline_factory.py
+++ ai/pipeline_factory.py
@@ -1,8 +1,10 @@
+from typing import Any, Dict, List
 from skl2onnx.sklapi import TraceableTfidfVectorizer # required for ONNX export!
 from skl2onnx import update_registered_converter
 from skl2onnx.shape_calculators.text_vectorizer import calculate_sklearn_text_vectorizer_output_shapes
 from skl2onnx.operator_converters.tfidf_vectoriser import convert_sklearn_tfidf_vectoriser
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 from sklearn.calibration import CalibratedClassifierCV
@@ -25,34 +27,18 @@ def create_vectorizer():
   )
 
   return TraceableTfidfVectorizer(
-    ngram_range=(1, 3),          # 1–4 n-grams
     lowercase=True,
     token_pattern=r"[a-z0-9-]+", # word tokenization (words only no spaces)
     stop_words=None,
     sublinear_tf=True)
 
 # Train data set using Logistic Regression
-def create_lr_pipeline(random_state: int = 42,
-    C: int = 8,
-    tol: float = 0.004,
-    max_iter: int = 30) -> Pipeline:
-
-  print("Building training Logistic Regression pipeline...")
-  print(f"C={C}, tol={tol}, max_iter={max_iter}")
-
+def create_lr_pipeline() -> Pipeline:
   # see https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
   clf = LogisticRegression(
     solver="lbfgs",
-    max_iter=max_iter,
-    # ignored for non-stochastic
-    random_state=random_state,
     penalty="l2",
-    C=C,
-    # convergence tolerance
-    tol=tol,
-    verbose=0,
-    # use all cores
-    n_jobs=1)
+    n_jobs=-1)
 
   return Pipeline(steps=[
     ("tfidf", create_vectorizer()),
@@ -93,3 +79,21 @@ def create_svm_pipeline(random_state: int = 42,
     ("tfidf", create_vectorizer()),
     ("classifier", clf)
   ])
+
+def create_hyperparams_search(estimator_pipeline: Pipeline, param_grid: Dict[str, List[Any]]):
+
+  scoring = {
+    "neg_log_loss": "neg_log_loss",
+    "accuracy": "accuracy",
+  }
+
+  return GridSearchCV(
+    estimator=estimator_pipeline,
+    param_grid=param_grid,
+    scoring=scoring,
+    refit="neg_log_loss",     # best model = lowest log loss
+    cv=5,
+    verbose=3,
+    return_train_score=False,
+    n_jobs=-1
+  )
