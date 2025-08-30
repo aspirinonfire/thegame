a39a78f255314e4d54e9db7f034cca3ac907e022
Alex Chernyak <alexchernyak@gmail.com>
2025-08-30 11:56:51 -0700

    better param search

--- ai/trainer.py
+++ ai/trainer.py
@@ -3,14 +3,15 @@ from typing import Any, Dict, List
 from sklearn.model_selection import train_test_split
 import os
 import argparse
+from scipy.stats import loguniform
 
 from sklearn.pipeline import Pipeline
 
 from data_loader import read_raw_data, transform_to_training_rows
 from evaluator import compute_model_evaluations
-from hyperparam_manager import find_best_lr_params, load_hyperparams_from_file, save_hyperparams_to_file
+from hyperparam_manager import find_best_params_grid_search, find_best_params_random_halving_search, load_hyperparams_from_file, save_hyperparams_to_file
 from model_utils import export_to_onnx, print_top_k
-from pipeline_factory import CLF_STEP, VEC_STEP, create_lr_pipeline;
+from pipeline_factory import CLF_STEP, VEC_STEP, create_lr_pipeline, create_svm_pipeline;
 
 def main(use_search: bool,
   training_data_path: str,
@@ -58,7 +59,7 @@ def main(use_search: bool,
   # get_feature_contribs(estimator, ["us-al", "us-nh", "us-tn", "us-vt"], "green background")
 
   # save to onnx
-  export_to_onnx(final_estimator, onnx_export_path)
+  # export_to_onnx(final_estimator, onnx_export_path)
 
 
 def create_lr_estimator_from_precomputed_params(X_train: Any,
@@ -100,28 +101,46 @@ def create_lr_estimator_from_search(X_train: Any,
   random_state: int,
   lr_training_params_path: str) -> Pipeline:
   
-  lr_pipeline = create_lr_pipeline(random_state, max_iter=1000)
+  lr_pipeline = create_lr_pipeline(random_state, max_iter=10000)
 
-  param_grid: Dict[str, List[Any]] = {
+  lr_param_distr: Dict[str, List[Any]] = {
     # Vectorizer
     f"{VEC_STEP}__ngram_range": [(1, 1), (1, 2)],
     f"{VEC_STEP}__use_idf": [True, False],
     f"{VEC_STEP}__norm": [None],
-    f"{VEC_STEP}__sublinear_tf": [False],
+    f"{VEC_STEP}__sublinear_tf": [True, False],
     # Classifier
-    f"{CLF_STEP}__solver": ["lbfgs", "saga"],
-    f"{CLF_STEP}__tol": [0.001, 0.0005, 0.0001, 0.00005],
-    f"{CLF_STEP}__C": [5.0, 7.0, 8.0, 9.0, 10.0, 20.0, 50.0, 70.0],
-    f"{CLF_STEP}__class_weight": [None]
+    f"{CLF_STEP}__solver": ["lbfgs"],
+    f"{CLF_STEP}__tol": loguniform(0.00001, 0.01),
+    f"{CLF_STEP}__C": loguniform(0.0001, 1000),
+    f"{CLF_STEP}__class_weight": [None, "balanced"]
   }
 
-  search_results = find_best_lr_params(pipeline=lr_pipeline,
+  # svc_pipeline = create_svm_pipeline(random_state, max_iter=1000)
+  # svc_param_distr: Dict[str, List[Any]] = {
+  #   # Vectorizer
+  #   f"{VEC_STEP}__ngram_range": [(1, 1), (1, 2)],
+  #   f"{VEC_STEP}__use_idf": [True, False],
+  #   f"{VEC_STEP}__norm": [None],
+  #   f"{VEC_STEP}__sublinear_tf": [True, False],
+  #   # Classifier
+  #   f"{CLF_STEP}__estimator__C": loguniform(0.001, 1000),
+  #   f"{CLF_STEP}__estimator__tol": loguniform(0.00001, 0.001),
+  #   f"{CLF_STEP}__estimator__dual": [True, False],
+  #   f"{CLF_STEP}__estimator__class_weight": [None, "balanced"],
+  #   # Calibration method
+  #   f"{CLF_STEP}__method": ["sigmoid", "isotonic"],
+  #   f"{CLF_STEP}__cv": [5],
+  # }
+
+  search_results = find_best_params_random_halving_search(pipeline=lr_pipeline,
     X_train=X_train,
     X_test=X_test,
     y_train=y_train,
     y_test=y_test,
     random_state=random_state,
-    param_grid=param_grid,
+    param_distributions=lr_param_distr,
+    resource=f"{CLF_STEP}__max_iter",
     refit="ndcg")
   
   search_results.print_results()
